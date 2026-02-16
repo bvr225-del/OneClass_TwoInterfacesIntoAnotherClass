bc79832792862c66bea1d8e8113950e93ea3bdd0
venkat <dell@DESKTOP-S8CP3CH>
2026-02-16 11:06:21 +0530

    Add project files.

---
+++ OneClass_TwoInterfacesIntoAnotherClass/NokiaAsha.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneClass_TwoInterfacesIntoAnotherClass
+{
+    // internal class NokiaAsha : INokia1, INokia2, MyClass
+    //NOTE:MyClass ALWAYS WRITTEN BEFORE INTERFACES IN INHERITANCE PROCESS.OTHERWISE IT WILL THROW ERROR.
+    internal class NokiaAsha : MyClass, INokia1, INokia2
+    {
+        public void Bluetooth()
+        {
+            Console.WriteLine("I am using blueetooth");
+        }
+
+        public void calling()
+        {
+            Console.WriteLine("Calling from NokiaAsha MOBILE");
+        }
+
+        public void endcall()
+        {
+            Console.WriteLine("call ended by NokiaAsha");
+        }
+
+        public void receiving()
+        {
+            Console.WriteLine("call recieved by NokiaAsha");
+        }
+
+        public void sendmessage()
+        {
+            Console.WriteLine("message is sending from nokiaAsha");
+        }
+
+        public void Wifi()
+        {
+            Console.WriteLine("call  by Wifi");
+        }
+    }
+}
---
+++ OneClass_TwoInterfacesIntoAnotherClass/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneClass_TwoInterfacesIntoAnotherClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            INokia1 objinokia1 = new Nokia1100();
+            objinokia1.calling();
+            objinokia1.receiving();
+            objinokia1.endcall();
+            objinokia1.sendmessage();
+            objinokia1 = new NokiaAsha();
+            objinokia1.calling();
+            objinokia1.receiving();
+            objinokia1.endcall();
+            objinokia1.sendmessage();
+            objinokia1 = null;
+            INokia2 objinokia2 = new NokiaAsha();
+            objinokia2.Bluetooth();
+            objinokia2.Wifi();
+            objinokia2 = null;
+            NokiaAsha objinokiaasha = new NokiaAsha();
+            objinokiaasha.Wifi();
+            objinokiaasha.print();
+
+            //==================(OR)//SECOND WAY CALLING METHODS BY USING CREATE DIRECT OBJECT OF THE CHILD CLASS====================
+            //Create the object for child classes and we can access those methods.
+            Nokia1100 nokia1100Obj = new Nokia1100();
+            nokia1100Obj.calling();
+            nokia1100Obj.endcall();
+            nokia1100Obj.sendmessage();
+            nokia1100Obj.receiving();
+
+
+            NokiaAsha nokiaAshaObj = new NokiaAsha();
+
+            nokiaAshaObj.calling();
+            nokiaAshaObj.sendmessage();
+            nokiaAshaObj.receiving();
+            nokiaAshaObj.endcall();
+            nokiaAshaObj.Wifi();
+            nokiaAshaObj.Bluetooth();
+            nokiaAshaObj.print();
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+/*
+ * OUTPUT:
+ * CALLING FROM NOKIA1100 MOBILE
+call recieved by 1100
+call ended by 1100
+message is sending from nokia1100
+Calling from NokiaAsha MOBILE
+call recieved by NokiaAsha
+call ended by NokiaAsha
+message is sending from nokiaAsha
+I am using blueetooth
+call  by Wifi
+call  by Wifi
+print is calling
+CALLING FROM NOKIA1100 MOBILE
+call ended by 1100
+message is sending from nokia1100
+call recieved by 1100
+Calling from NokiaAsha MOBILE
+message is sending from nokiaAsha
+call recieved by NokiaAsha
+call ended by NokiaAsha
+call  by Wifi
+I am using blueetooth
+print is calling
+*/
